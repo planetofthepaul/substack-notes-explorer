8806fff89c9514eb942718e368834d5e7af01c6a
planetofthepaul <planetofthepaul@gmail.com>
2025-12-23 15:06:11 -0500

    Integrate Plausible analytics into app.py
    Added Plausible analytics script for tracking.

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ HTML = '''
 <html>
 <head>
     <title>Substack Notes Exporter</title>
+    <!-- Privacy-friendly analytics by Plausible -->
+    <script async src="https://plausible.io/js/pa-jluo04lsBer5Kjum87QyY.js"></script>
+    <script>
+      window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+      plausible.init()
+    </script>
     <style>
         * { box-sizing: border-box; }
         body { 
